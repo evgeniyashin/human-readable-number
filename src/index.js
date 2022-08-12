fbb010a0020b68098a7f0b49362e62749f643775
evgeniyashin <106561672+evgeniyashin@users.noreply.github.com>
2022-08-13 02:59:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-
+ let ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let tens = ['0', '0', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  //let hundreds = ['hundred']
+  number = number.toString();
+  //console.log(number)
+  if (number < 20) return ones[number];
+  if (number >= 20 && number < 100) {
+    if (number % 10 === 0) return `${tens[number[0]]}`
+    else return `${tens[number[0]]} ${ones[number[1]]}`
+  }
+  if (number >= 100 && number < 1000) {
+    if (number % 100 === 0) return `${ones[number[0]]} hundred`
+    else if (number.slice(1) < 20) {
+      subNum = +(number.slice(1));
+      return `${ones[number[0]]} hundred ${ones[subNum]}`}
+      else if (number.slice(1) % 10 === 0) return `${ones[number[0]]} hundred ${tens[number[1]]}`
+      else return `${ones[number[0]]} hundred ${tens[number[1]]} ${ones[number[2]]}`
+  }
 }
